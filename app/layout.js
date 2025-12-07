c436ae913b4f7bb320e7af2cdcaba9bdce748112
Esteb4ncd <davidecruzd10@gmail.com>
2025-12-07 05:34:02 -0800

    refining: fonts and spacing

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
